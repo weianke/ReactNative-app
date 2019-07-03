ceb5a1e5a5181bbba89bfea1513e65b2b3702cba
anke <245783065@qq.com>
2019-07-03 18:18:01 +0800

    完成三个页面的效果

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,198 +1,100 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { Component } from 'react'
+import { Image, FlatList, StyleSheet, Text, View } from 'react-native'
 
-import { MonoText } from '../components/StyledText';
+const REQUEST_URL =
+  'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json'
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+export default class homeScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      loaded: false
+    }
+  }
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+  componentDidMount() {
+    this.fetchData()
+  }
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then(response => response.json())
+      .then(resData => {
+        this.setState({
+          data: resData.movies,
+          loaded: true
+        })
+      })
+  }
 
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView()
+    }
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={StyleSheet.list}
+        keyExtractor={item => item.id}
+      />
+    )
+  }
 
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading Movie...</Text>
+      </View>
+    )
+  }
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
+  renderMovie({ item }) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: item.posters.thumbnail }}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightCcontainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.year}>{item.year}</Text>
         </View>
       </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
+    )
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    backgroundColor: '#F5FCFF'
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  rightCcontainer: {
+    flex: 1
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center'
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  year: {
+    textAlign: 'center'
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+  thumbnail: {
+    width: 53,
+    height: 81
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
+  }
+})
+
+homeScreen.navigationOptions = {
+  header: null
+}
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React, { Component } from 'react'
+import axios from 'axios'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  FlatList,
+  ScrollView
+} from 'react-native'
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+const baseUrl =
+  'https://easy-mock.com/mock/5d1c53247abfb76fe0923cee/RnMovie/movie'
+export default class settings extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  async getData() {
+    const res = await axios.get(baseUrl)
+    console.log(res)
+    this.setState({
+      data: res.data.data.List
+    })
+  }
+
+  render() {
+    let list
+    if (this.state.data && this.state.data.length > 0) {
+      list = this.state.data.map((item, index) => {
+        return (
+          <View style={styles.container} key={index}>
+            <Image source={{ uri: item.pictues }} style={styles.thumbnail} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.year}>{item.year}</Text>
+            </View>
+          </View>
+        )
+      })
+    } else {
+      list = (
+        <View style={styles.container}>
+          <Text>Loading movies...</Text>
+        </View>
+      )
+    }
+    return (
+      <ScrollView>
+        <View>{list}</View>
+      </ScrollView>
+    )
+  }
 }
 
-SettingsScreen.navigationOptions = {
-  title: 'app.json',
-};
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center'
+  },
+  year: {
+    textAlign: 'center'
+  },
+  thumbnail: {
+    marginLeft: 15,
+    width: 53,
+    height: 81
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
+  }
+})
+
+// 配置头部导航
+settings.navigationOptions = {
+  header: null
+}
